8a70858924e4a7028636488d509eae0a60e51c34
BustDot <Bust.dev@outlook.com>
2023-08-06 11:03:54 +0800

    feat: complete graphql-authz

--- authz/utils.py
+++ authz/utils.py
@@ -2,7 +2,7 @@ from graphql import GraphQLResolveInfo
 
 
 def info_to_path(info: GraphQLResolveInfo) -> str:
-    """"Get the full path of current endpoint by GraphQLResolveInfo."""
+    """ "Get the full path of current endpoint by GraphQLResolveInfo."""
     node = info.path
     full_path = node.key
     while node.prev:
--- tests/test_graphql_authz.py
+++ tests/test_graphql_authz.py
@@ -100,8 +100,7 @@ class TestConfig(TestCase):
         return GraphQLSchema(query_type)
 
     def given_an_enforcer(self):
-        return casbin.Enforcer(get_examples("model.conf"), get_examples("policy.csv")
-        )
+        return casbin.Enforcer(get_examples("model.conf"), get_examples("policy.csv"))
 
     def test_graphql_middleware(self):
         schema = self.given_a_graphql_schema()
@@ -119,82 +118,126 @@ class TestConfig(TestCase):
         casbin_middleware = enforcer_middleware(enforcer)
 
         response = graphql_sync(
-            schema, query, middleware=[casbin_middleware], context_value={"role": "user"})
+            schema,
+            query,
+            middleware=[casbin_middleware],
+            context_value={"role": "user"},
+        )
 
-        self.assertEqual(response.errors[0].formatted, {
-            "message": "user can not query project.name",
-            "path": ['project', 'name'],
-            "locations": [{'line': 3, 'column': 20}]
-        })
+        self.assertEqual(
+            response.errors[0].formatted,
+            {
+                "message": "user can not query project.name",
+                "path": ["project", "name"],
+                "locations": [{"line": 3, "column": 20}],
+            },
+        )
 
-        self.assertEqual(response.errors[1].formatted, {
-            "message": "user can not query project.members.tickets.message",
-            "path": ['project', 'members', 0, 'tickets', 0, 'message'],
-            "locations": [{'line': 5, 'column': 28}]
-        })
+        self.assertEqual(
+            response.errors[1].formatted,
+            {
+                "message": "user can not query project.members.tickets.message",
+                "path": ["project", "members", 0, "tickets", 0, "message"],
+                "locations": [{"line": 5, "column": 28}],
+            },
+        )
 
-        self.assertEqual(response.errors[2].formatted, {
-            "message": "user can not query project.members.tickets.message",
-            "path": ['project', 'members', 0, 'tickets', 1, 'message'],
-            "locations": [{'line': 5, 'column': 28}]
-        })
+        self.assertEqual(
+            response.errors[2].formatted,
+            {
+                "message": "user can not query project.members.tickets.message",
+                "path": ["project", "members", 0, "tickets", 1, "message"],
+                "locations": [{"line": 5, "column": 28}],
+            },
+        )
 
-        self.assertEqual(response.errors[3].formatted, {
-            "message": "user can not query project.members.tickets.message",
-            "path": ['project', 'members', 0, 'tickets', 2, 'message'],
-            "locations": [{'line': 5, 'column': 28}]
-        })
+        self.assertEqual(
+            response.errors[3].formatted,
+            {
+                "message": "user can not query project.members.tickets.message",
+                "path": ["project", "members", 0, "tickets", 2, "message"],
+                "locations": [{"line": 5, "column": 28}],
+            },
+        )
 
-        self.assertEqual(response.errors[4].formatted, {
-            "message": "user can not query project.members.tickets.message",
-            "path": ['project', 'members', 0, 'tickets', 3, 'message'],
-            "locations": [{'line': 5, 'column': 28}]
-        })
+        self.assertEqual(
+            response.errors[4].formatted,
+            {
+                "message": "user can not query project.members.tickets.message",
+                "path": ["project", "members", 0, "tickets", 3, "message"],
+                "locations": [{"line": 5, "column": 28}],
+            },
+        )
 
-        self.assertEqual(response.errors[5].formatted, {
-            "message": "user can not query project.members.tickets.message",
-            "path": ['project', 'members', 1, 'tickets', 0, 'message'],
-            "locations": [{'line': 5, 'column': 28}]
-        })
+        self.assertEqual(
+            response.errors[5].formatted,
+            {
+                "message": "user can not query project.members.tickets.message",
+                "path": ["project", "members", 1, "tickets", 0, "message"],
+                "locations": [{"line": 5, "column": 28}],
+            },
+        )
 
-        self.assertEqual(response.errors[6].formatted, {
-            "message": "user can not query project.members.tickets.message",
-            "path": ['project', 'members', 1, 'tickets', 1, 'message'],
-            "locations": [{'line': 5, 'column': 28}]
-        })
+        self.assertEqual(
+            response.errors[6].formatted,
+            {
+                "message": "user can not query project.members.tickets.message",
+                "path": ["project", "members", 1, "tickets", 1, "message"],
+                "locations": [{"line": 5, "column": 28}],
+            },
+        )
 
-        self.assertEqual(response.errors[7].formatted, {
-            "message": "user can not query project.members.tickets.message",
-            "path": ['project', 'members', 1, 'tickets', 2, 'message'],
-            "locations": [{'line': 5, 'column': 28}]
-        })
+        self.assertEqual(
+            response.errors[7].formatted,
+            {
+                "message": "user can not query project.members.tickets.message",
+                "path": ["project", "members", 1, "tickets", 2, "message"],
+                "locations": [{"line": 5, "column": 28}],
+            },
+        )
 
-        self.assertEqual(response.errors[8].formatted, {
-            "message": "user can not query project.members.tickets.message",
-            "path": ['project', 'members', 1, 'tickets', 3, 'message'],
-            "locations": [{'line': 5, 'column': 28}]
-        })
+        self.assertEqual(
+            response.errors[8].formatted,
+            {
+                "message": "user can not query project.members.tickets.message",
+                "path": ["project", "members", 1, "tickets", 3, "message"],
+                "locations": [{"line": 5, "column": 28}],
+            },
+        )
 
-        self.assertEqual(response.data, {
-            'project': {
-                'id': 2,
-                'name': None,
-                'members': [{
-                    'id': 1, 'name': 'Project 2, Member: 1',
-                    'tickets': [{'id': 1, 'message': None}, {'id': 2, 'message': None},
-                                {'id': 3, 'message': None}, {'id': 4, 'message': None}]
-                }, {
-                    'id': 2, 'name': 'Project 2, Member: 2',
-                    'tickets': [{'id': 1, 'message': None}, {'id': 2, 'message': None},
-                                {'id': 3, 'message': None}, {'id': 4, 'message': None}]
-                }]
-            }
-        })
+        self.assertEqual(
+            response.data,
+            {
+                "project": {
+                    "id": 2,
+                    "name": None,
+                    "members": [
+                        {
+                            "id": 1,
+                            "name": "Project 2, Member: 1",
+                            "tickets": [
+                                {"id": 1, "message": None},
+                                {"id": 2, "message": None},
+                                {"id": 3, "message": None},
+                                {"id": 4, "message": None},
+                            ],
+                        },
+                        {
+                            "id": 2,
+                            "name": "Project 2, Member: 2",
+                            "tickets": [
+                                {"id": 1, "message": None},
+                                {"id": 2, "message": None},
+                                {"id": 3, "message": None},
+                                {"id": 4, "message": None},
+                            ],
+                        },
+                    ],
+                }
+            },
+        )
 
-    @sub_test([
-        dict(context={"role": "*"}),
-        dict(context={})
-    ])
+    @sub_test([dict(context={"role": "*"}), dict(context={})])
     def test_graphql_middleware_as_anonymous(self, context):
         schema = self.given_a_graphql_schema()
         enforcer = self.given_an_enforcer()
@@ -206,20 +249,25 @@ class TestConfig(TestCase):
         }"""
         casbin_middleware = enforcer_middleware(enforcer)
 
-        response = graphql_sync(schema, query, middleware=[casbin_middleware], context_value=context)
+        response = graphql_sync(
+            schema, query, middleware=[casbin_middleware], context_value=context
+        )
 
-        self.assertEqual(response.errors[0].formatted, {
-            "message": "anonymous can not query project.name",
-            "path": ['project', 'name'],
-            "locations": [{'line': 3, 'column': 20}]
-        })
-        self.assertEqual(response.data, {'project': {'id': 2, 'name': None}})
+        self.assertEqual(
+            response.errors[0].formatted,
+            {
+                "message": "anonymous can not query project.name",
+                "path": ["project", "name"],
+                "locations": [{"line": 3, "column": 20}],
+            },
+        )
+        self.assertEqual(response.data, {"project": {"id": 2, "name": None}})
 
     def test_graphql_middleware_unauthorized_querying_non_nullable_fields(self):
         schema = self.given_a_graphql_schema()
         enforcer = casbin.Enforcer(
             get_examples("model.conf"),
-            get_examples("policy_with_project_id_restricted.csv")
+            get_examples("policy_with_project_id_restricted.csv"),
         )
 
         query = """{
@@ -229,12 +277,19 @@ class TestConfig(TestCase):
         }"""
         casbin_middleware = enforcer_middleware(enforcer)
 
-        response = graphql_sync(schema, query, middleware=[casbin_middleware],
-                                context_value={"role": "unathorized_user"})
+        response = graphql_sync(
+            schema,
+            query,
+            middleware=[casbin_middleware],
+            context_value={"role": "unathorized_user"},
+        )
 
-        self.assertEqual(response.errors[0].formatted, {
-             "message": "unathorized_user can not query project.id",
-             "path": ['project', 'id'],
-             "locations": [{'line': 3, 'column': 17}]
-        })
-        self.assertEqual(response.data, {'project': None})
+        self.assertEqual(
+            response.errors[0].formatted,
+            {
+                "message": "unathorized_user can not query project.id",
+                "path": ["project", "id"],
+                "locations": [{"line": 3, "column": 17}],
+            },
+        )
+        self.assertEqual(response.data, {"project": None})
